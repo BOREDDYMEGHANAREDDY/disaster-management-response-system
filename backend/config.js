be60b608435191112f54a6ccadb432826345f2b2
BOREDDY MEGHANA REDDY <meghanareddyboreddy@gmail.com>
2026-04-14 16:51:43 +0530

    Removed localhost MongoDB connection

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/disasterDB")
+  .connect(process.env.MONGO_URI)
   .then(() =>
     console.log("MongoDB Connected")
   )
   .catch((err) =>
-    console.log(err)
+    console.log("MongoDB Error:", err)
   );
 
 module.exports = mongoose;
